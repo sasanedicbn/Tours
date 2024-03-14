418edc386039a4b37d273cf88dc8abb1ac730821
Sasa Nedic <svetasmak0@gmail.com>
2024-03-14 13:21:37 +0100

    Add useEffect state

--- Tours/src/components/Tours.jsx
+++ Tours/src/components/Tours.jsx
@@ -1,9 +1,25 @@
 import Tour from "./Tour";
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 
 const Tours = () => {
     const [tours, setTours] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://course-api.com/react-tours-project');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setTours(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData(); 
+    }, []); 
 
     function removeTour(id) {
         const updatedTours = tours.filter((tour) => tour.id !== id);
