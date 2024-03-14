21dfe289f826e05782b928976016b27cee40250c
Sasa Nedic <svetasmak0@gmail.com>
2024-03-14 18:36:28 +0100

    Create style for RefetchData

--- Tours/src/components/RefetchData.jsx
+++ Tours/src/components/RefetchData.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RefetchData = ({ refetchData }) => {
   return (
     <div className="refetch-container">
-      <h1 className="refetch-title">Refetch Data</h1>
+      <h1 className="refetch-title">No tours left!</h1>
       <button onClick={() => refetchData()} className="refetch-button">
         Refetch Data
       </button>
